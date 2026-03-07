02b60b8b814bf3ecf03474ad0a9ab1b36df1325b
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2026-03-07 15:36:40 +0700

    Updated

--- src/main/java/com/project/droneDeliverySystem/config/SecurityConfig.java
+++ src/main/java/com/project/droneDeliverySystem/config/SecurityConfig.java
@@ -40,10 +40,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf
-                .ignoringRequestMatchers("/api/**")
-        );
-
                 http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**",
                                 "/css/**",
